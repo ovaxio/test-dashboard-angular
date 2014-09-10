a4d4337082862f64063fea5209bfc522ed7d27f6
Magicshui <shuiyuzhe@gmail.com>
2014-09-10 15:01:57 +0800

    Separate  the FE and BE

--- BE/main.go
+++ BE/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	_ "github.com/magicshui/pm-lucky/routers"
+	_ "github.com/magicshui/pm-lucky/BE/routers"
 )
 
 func main() {
--- BE/routers/router.go
+++ BE/routers/router.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/magicshui/pm-lucky/controllers"
+	"github.com/magicshui/pm-lucky/BE/controllers"
 )
 
 func init() {
