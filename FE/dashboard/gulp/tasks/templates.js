ad19747aa487f74cf958d17e3a8b51a7c58711a7
ovaxio <ovaxio@gmail.com>
2014-09-12 15:02:57 +0800

    update gulp tasks

--- FE/dashboard/gulp/tasks/templates.js
+++ FE/dashboard/gulp/tasks/templates.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
   plumber = require('gulp-plumber'),
   jade = require('gulp-jade'),
+  rename = require('gulp-rename'),
   size = require('gulp-size');
 
 gulp.task('templates', function () {
@@ -8,5 +9,8 @@ gulp.task('templates', function () {
     .pipe(plumber())
     .pipe(jade({pretty: false}))
     .pipe(size())
+    .pipe(rename(function (path) {
+      path.dirname = path.dirname.split('/').shift();
+    }))
     .pipe(gulp.dest('dist/templates'));
 });
