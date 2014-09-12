7eec088f426297b2afd1aa52e82e078a02dbbb11
ovaxio <ovaxio@gmail.com>
2014-09-12 19:23:35 +0800

    update gulp task

--- FE/dashboard/gulp/tasks/bower.js
+++ FE/dashboard/gulp/tasks/bower.js
@@ -6,11 +6,18 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('bower', function () {
-  return gulp.src(gulpBowerFiles(), {base: 'bower_components'})
+  gulp.src(gulpBowerFiles({filter: /(\.js$)/i}), {base: 'bower_components'})
     .pipe(sourcemaps.init())
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(size())
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest("dist/js"));
+
+  gulp.src(gulpBowerFiles({filter: /(\.css$)/i}), {base: 'bower_components'})
+    .pipe(sourcemaps.init())
+    .pipe(concat('vendor.min.css'))
+    .pipe(size())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest("dist/css"));
 });
