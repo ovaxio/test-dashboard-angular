e5e76b6b6590c4d36735f246c104d96acd6a7e9e
ovaxio <ovaxio@gmail.com>
2014-09-12 16:29:25 +0800

    add sourcemap for coffee and stylus for debugging

--- FE/dashboard/gulp/tasks/bower.js
+++ FE/dashboard/gulp/tasks/bower.js
@@ -2,12 +2,15 @@ var gulp = require('gulp'),
   gulpBowerFiles = require('main-bower-files'),
   uglify = require('gulp-uglify'),
   concat = require('gulp-concat'),
-  size = require('gulp-size');
+  size = require('gulp-size'),
+  sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('bower', function () {
   return gulp.src(gulpBowerFiles(), {base: 'bower_components'})
+    .pipe(sourcemaps.init())
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(size())
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest("dist/js"));
 });
